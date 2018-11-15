6f7f2a65b4a4b6f139b2e8c27da16c1a942eeb01
gfranke <gfranke@w3sdesign.com>
2018-11-15 23:37:08 +0100

    ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CustomerEditComponent } from './customer/customer-edit/customer-edit.component';
+import { CustomerListComponent } from './customer/customer-list/customer-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomerEditComponent,
+    CustomerListComponent
   ],
   imports: [
     BrowserModule,
